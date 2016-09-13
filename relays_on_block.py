72d407c65df4131fce25a292600e5c399f778349
Simon Howroyd <howroydlsu@gmail.com>
2016-09-13 09:17:42 +0100

    Relay script working well

--- relays_on_block.py
+++ relays_on_block.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 import RPi.GPIO as gpio
+import datetime
 
-pins = (17, 22, 23)
+on = 9
+off = 18
+
+pins = (22, 23)
+lights = 17
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
+gpio.setup(lights, gpio.OUT)
+for x in pins:
+	gpio.setup(x, gpio.OUT)
+
+
 while True:
+	now = datetime.datetime.now()
+	if now.hour >= on and now.hour < off:
+		gpio.output(lights, 0)
+	else:
+		gpio.output(lights, 1)
+
 	for x in pins:
-		gpio.setup(x, gpio.OUT)
 		gpio.output(x, 0)
 
