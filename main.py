54263f27b590a81877f7de1ca09b35060172c324
Simon Howroyd <howroydlsu@gmail.com>
2016-09-25 20:51:03 +0100

    Added CPU temperature monitoring

--- main.py
+++ main.py
@@ -3,54 +3,69 @@ import RPi.GPIO as gpio
 import smbus
 import datetime
 import time
-
-water_change = False
+import os
+from enum import Enum
 
 bus = smbus.SMBus(1)
 
 address_nano = 0x69
 
-on = 9
-off = 21
+time_lights_on = 9
+time_lights_off = 21
+
+class reg(Enum):
+    red = 1
+    green = 2
+    blue = 3
+    fan = 4
+    filter = 5
+    lights = 6
+    heater = 7
+    air = 8
+
+# Return CPU temperature as a character string                                      
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return(float(res.replace("temp=","").replace("'C\n","")))
+
+time_fan_on = time.time()
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
-bus.write_byte_data(address_nano, 1, 255)  # Fan
-bus.write_byte_data(address_nano, 2, 255)  # Fan
-bus.write_byte_data(address_nano, 3, 255)  # Fan
-bus.write_byte_data(address_nano, 4, 255)  # Fan
-#bus.write_byte_data(address_nano, 5, True) # Filter
-#bus.write_byte_data(address_nano, 6, True) # Lights
-#bus.write_byte_data(address_nano, 7, True) # Heater
-#bus.write_byte_data(address_nano, 8, True) # Air
+bus.write_byte_data(address_nano, reg['red'].value, 255)  # Red
+bus.write_byte_data(address_nano, reg['green'].value, 255)  # Green
+bus.write_byte_data(address_nano, reg['blue'].value, 255)  # Blue
+bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
+bus.write_byte_data(address_nano, reg['filter'].value, True) # Filter
+bus.write_byte_data(address_nano, reg['lights'].value, True) # Lights
+bus.write_byte_data(address_nano, reg['heater'].value, False) # Heater
+bus.write_byte_data(address_nano, reg['air'].value, True) # Air
 
 while True:
 	try:
 		now = datetime.datetime.now()
 
-		if now.hour >= on and now.hour < off:
+		if now.hour >= time_lights_on and now.hour < time_lights_off:
 			bus.write_byte_data(address_nano, 6, True) # Lights
 		else:
 			bus.write_byte_data(address_nano, 6, False) # Lights
 
-		if water_change is True:
-			bus.write_byte_data(address_nano, 5, False) # Filter
-			bus.write_byte_data(address_nano, 7, False) # Heater
-		else:
-			bus.write_byte_data(address_nano, 5, True) # Filter
-			bus.write_byte_data(address_nano, 7, True) # Heater
-		bus.write_byte_data(address_nano, 8, True) # Air
+		bus.write_byte_data(address_nano, reg['filter'].value, True) # Filter
+		bus.write_byte_data(address_nano, reg['heater'].value, True) # Heater
+		bus.write_byte_data(address_nano, reg['air'].value, True) # Air
 
 		time.sleep(1)
 
 		data = bus.read_i2c_block_data(address_nano, 0)
 
-#		for i in range(9):
-#			print(str(data[i]), end=',')
-#		print()
-
-#		time.sleep(1)
-
+		temp = getCPUtemperature()
+		print(temp, "degC")
+		if (getCPUtemperature() > 50.0):
+			time_fan_on = time.time()
+			bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
+		elif (time.time() > 30):
+			bus.write_byte_data(address_nano, reg['fan'].value, 0)  # Fan
+			
 	except OSError:
 		pass
