7077caf0d8d29d3d274f4d5bb9879d42981efeb4
Simon Howroyd <howroydlsu@gmail.com>
2016-09-26 19:35:25 +0100

    Broke it

--- main.py
+++ main.py
@@ -33,39 +33,61 @@ time_fan_on = time.time()
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 
-bus.write_byte_data(address_nano, reg['red'].value, 255)  # Red
-bus.write_byte_data(address_nano, reg['green'].value, 255)  # Green
-bus.write_byte_data(address_nano, reg['blue'].value, 255)  # Blue
-bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
-bus.write_byte_data(address_nano, reg['filter'].value, True) # Filter
-bus.write_byte_data(address_nano, reg['lights'].value, True) # Lights
-bus.write_byte_data(address_nano, reg['heater'].value, False) # Heater
-bus.write_byte_data(address_nano, reg['air'].value, True) # Air
+#bus.write_byte_data(address_nano, 1, 255)  # Red
+#bus.write_byte_data(address_nano, 2, 255)  # Green
+#bus.write_byte_data(address_nano, 3, 255)  # Blue
+#bus.write_byte_data(address_nano, 4, 255)  # Fan
+#bus.write_byte_data(address_nano, 5, True) # Filter
+#bus.write_byte_data(address_nano, 6, False) # Lights
+#bus.write_byte_data(address_nano, 7, True) # Heater
+#bus.write_byte_data(address_nano, 8, True) # Air
+
+#time.sleep(5)
+
+#bus.write_byte_data(address_nano, reg['red'].value, 255)  # Red
+#bus.write_byte_data(address_nano, reg['green'].value, 255)  # Green
+#bus.write_byte_data(address_nano, reg['blue'].value, 255)  # Blue
+#bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
+#bus.write_byte_data(address_nano, reg['filter'].value, True) # Filter
+#bus.write_byte_data(address_nano, reg['lights'].value, True) # Lights
+#bus.write_byte_data(address_nano, reg['heater'].value, False) # Heater
+#bus.write_byte_data(address_nano, reg['air'].value, True) # Air
+
+test = [10,255,255,255,1,0,1,1]
 
 while True:
 	try:
 		now = datetime.datetime.now()
 
-		if now.hour >= time_lights_on and now.hour < time_lights_off:
-			bus.write_byte_data(address_nano, 6, True) # Lights
-		else:
-			bus.write_byte_data(address_nano, 6, False) # Lights
+		data = bus.read_i2c_block_data(address_nano, 0)[0:9]
 
-		bus.write_byte_data(address_nano, reg['filter'].value, True) # Filter
-		bus.write_byte_data(address_nano, reg['heater'].value, True) # Heater
-		bus.write_byte_data(address_nano, reg['air'].value, True) # Air
+		if now.hour >= time_lights_on and now.hour < time_lights_off:
+			test[5] = True
+			#bus.write_byte_data(address_nano, 6, True) # Lights
+		else:
+			test[5] = False
+			#bus.write_byte_data(address_nano, 6, False) # Lights
+
+#		bus.write_byte_data(address_nano, 5, True) # Filter
+#		bus.write_byte_data(address_nano, 7, True) # Heater
+#		bus.write_byte_data(address_nano, 8, True) # Air
 
 		time.sleep(1)
+		if data != test:
+			print("New data")
+			for i in test:
+				bus.write_byte(address_nano,i)
 
-		data = bus.read_i2c_block_data(address_nano, 0)
 
 		temp = getCPUtemperature()
 		print(temp, "degC")
-		if (getCPUtemperature() > 50.0):
-			time_fan_on = time.time()
-			bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
-		elif (time.time() > 30):
-			bus.write_byte_data(address_nano, reg['fan'].value, 0)  # Fan
+#		if (getCPUtemperature() > 50.0):
+#			time_fan_on = time.time()
+#			bus.write_byte_data(address_nano, reg['fan'].value, 255)  # Fan
+#		elif (time.time() > 30):
+#			bus.write_byte_data(address_nano, reg['fan'].value, 0)  # Fan
+
+		print(data)
 			
-	except OSError:
+	except IOError:
 		pass
